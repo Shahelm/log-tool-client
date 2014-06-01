31aa7bfab6969426eff6a58128cd40f848256b28
alex.ma <alex.markovskiy.dev>
2014-06-01 21:33:26 +0300

    - moved generation phar archive in bin/creator-of-phar

--- creator.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'app/bootstrap.php';
-
-$phar = new Phar('log-tool-client.phar');
-
-$phar->buildFromDirectory('/var/www/log-tool-client/');
-
-$phar->setStub($phar->createDefaultStub('app/app.php'));
