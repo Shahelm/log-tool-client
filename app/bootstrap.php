764d87c89c60261219d9a52d7d17fa883a10ba78
alex.ma <alex.markovskiy.dev>
2015-06-11 22:20:22 +0300

    - add default timezone: 'America/Chicago'

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,3 +2,5 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 defined('ROOT_PATH') || define('ROOT_PATH', realpath(__DIR__  . DIRECTORY_SEPARATOR . '..'));
+
+date_default_timezone_set('America/Chicago');
